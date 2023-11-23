10e5887327faee58766ee8d9c4b17516da8a4dee
Dale Lane <dale.lane@uk.ibm.com>
2023-11-23 18:03:22 +0000

    feat: update eem-seed to use new REST API
    Update versions of Event Processing and Event Endpoint Management
    
    Rewrite of the eem-seed script to use the REST API in the new
    version of EEM.
    
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- eem-seed/reset-all-data.sh
+++ eem-seed/reset-all-data.sh
@@ -23,134 +23,25 @@ if [[ $BASH_SOURCE = */* ]]; then
     cd -- "${BASH_SOURCE%/*}/" || exit
 fi
 
-cookieCurlParams() {
-    USERNAME="${1}"
-    COOKIE_JAR="/tmp/${USERNAME}-cookies.txt"
-    echo -n "-b ${COOKIE_JAR} -c ${COOKIE_JAR}"
-}
-
-csrfHeader() {
-    USERNAME="${1}"
-    COOKIE_JAR="/tmp/${USERNAME}-cookies.txt"
-    CSRF=($(cat -v ${COOKIE_JAR} | grep "ibm-ei-csrf-token"))
-    CSRF_TOKEN="${CSRF[6]}"
-    CSRF_TOKEN="${CSRF_TOKEN//^M/}"
-    echo -n "x-ibm-ei-csrf-token: ${CSRF_TOKEN}"
-}
-
-login() {
-    echo "Logging in as ${1}"
-    USERNAME="${1}"
-    PASSWORD="${2}"
-    # count the number of times we have tried this
-    #  default to "first" attempt if not specified
-    ATTEMPT=${3:-1}
-
-    COOKIE_JAR="/tmp/${USERNAME}-cookies.txt"
-    HEADERS_FILE="/tmp/login-response-headers.txt"
-
-    # Trigger redirect and capture response
-    REDIRECT_RESPONSE=$(curl -f -k -s ${EEM_API} -c ${COOKIE_JAR} || true)
-    if [ -z "$REDIRECT_RESPONSE" ]
-    then
-        echo "Login unsuccessful."
-        if [[ $ATTEMPT -lt 5 ]]
-        then
-            echo "Retrying..."
-            sleep 2
-            login $USERNAME $PASSWORD $((ATTEMPT+=1))
-            return
-        else
-            exit 1
-        fi
-    fi
-
-    # Parse the response to identify callback URL
-    PARTS=($(echo $REDIRECT_RESPONSE | tr "?" " "))
-    QUERY=${PARTS[3]}
-    QUERY_PARAMS=($(echo $QUERY | tr "&" " "))
-
-    # Use response state to login as user and capture callback
-    curl -k -f "${EEM_API}/login/oauth/authorize/login" -F "username=${USERNAME}" -F "password=${PASSWORD}" -F ${QUERY_PARAMS[0]} -D ${HEADERS_FILE}
-    LOCATION=($(cat -v ${HEADERS_FILE} | grep location))
-    CALLBACK="${LOCATION[1]}"
-    CALLBACK="${CALLBACK//^M/}"
-    rm ${HEADERS_FILE}
-
-    # Call callback
-    curl -s -k "${CALLBACK}" $(cookieCurlParams ${USERNAME}) -o /dev/null
-
-    # Print user and grab CSRF
-    echo "Authenticated user"
-    curl -k "${EEM_API}/auth/protected/userinfo" $(cookieCurlParams ${USERNAME}) -s -o /dev/null
-}
+# check options provided
+if [ $# -ne 2 ]; then
+    >&2 echo "Usage: reset-all-data.sh <NAMESPACE> <ACCESS_TOKEN>"
+    exit 1
+fi
 
 # get the namespace from a command line argument
 NAMESPACE=$1
 
+# get the API access token from a command line argument
+ACCESS_TOKEN=$2
+
 # get location of the EEM manager (also useful for checking that oc login has been run)
-EEM_API=$(oc get route -n $NAMESPACE my-eem-manager-ibm-eem-manager -ojsonpath='https://{.spec.host}')
+EEM_API=$(oc get route -n $NAMESPACE my-eem-manager-ibm-eem-admin -ojsonpath='https://{.spec.host}')
 
-
-# check namespace option provided
-if [ $# -eq 0 ]; then
-    >&2 echo "Usage: reset-all-data.sh <NAMESPACE>"
-    exit 1
-fi
-
-
-# --------------------------------------
-# WARNING
-#
-#  ensure the user is aware the script
-#  will clear the EEM storage
-# --------------------------------------
 
 echo "========================================================================="
 echo " Event Automation tutorial setup "
-echo " ----------------------------------------------------------------------- "
-echo " This will CLEAR ALL STORAGE for the my-eem-manager instance "
-echo "  of Event Endpoint Management in the $NAMESPACE namespace "
-echo "  and replace it with definitions for the tutorial topics. "
 echo "========================================================================="
-read -p "Press Y to continue: " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-
-echo "..."
-
-# --------------------------------------
-# RESET
-#
-#  clear the EEM storage
-# --------------------------------------
-
-echo "> Clearing existing storage"
-
-oc exec -it -n $NAMESPACE my-eem-manager-ibm-eem-manager-0 -- rm -rf /opt/storage/eem
-oc delete pod -n $NAMESPACE my-eem-manager-ibm-eem-manager-0
-# waiting for replacement pod to be created
-until oc get pod -n $NAMESPACE my-eem-manager-ibm-eem-manager-0 >/dev/null 2>&1; do
-    sleep 1
-done
-# waiting for replacement pod to become ready
-oc wait -n $NAMESPACE --for=condition=ready pod my-eem-manager-ibm-eem-manager-0 --timeout=180s
-
-
-# --------------------------------------
-# Login
-#
-#  login with various users
-# --------------------------------------
-
-echo "> Logging into EEM manager"
-
-# TODO - get credentials dynamically
-CURRENT_USER=eem-admin
-PWD='Th1$ISTh3Adm1nPa$SW0Rd'
-login ${CURRENT_USER} ${PWD}
 
 
 # --------------------------------------
@@ -159,29 +50,59 @@ login ${CURRENT_USER} ${PWD}
 #  add the Event Streams cluster to EEM
 # --------------------------------------
 
-echo "> Submitting Event Streams cluster information"
+echo "> (1/3) Getting Event Streams cluster information"
 
-# get the certificates for the the Kafka cluster
-bootstrap_response=$(curl -s -X POST -k -k $(cookieCurlParams ${CURRENT_USER}) \
-    -H "$(csrfHeader ${CURRENT_USER})" \
-    -H "Content-Type: application/json" \
-    --data "{\"bootstrapServers\":[{\"host\":\"my-kafka-cluster-kafka-bootstrap.$NAMESPACE.svc\",\"port\":9095}]}" \
-    $EEM_API/api/discovery/validate-bootstrap )
+# get the certificates for the Kafka cluster
+ES_CERTIFICATE=$(oc get eventstreams my-kafka-cluster -o jsonpath='{.status.kafkaListeners[?(@.name=="authsslsvc")].certificates[0]}')
+ES_CERTIFICATE=${ES_CERTIFICATE//$'\n'/\\\\n}
 
-# add the credentials to the cluster definition
-credentials=$(cat eem-01-credentials.tmpl | sed "s/PLACEHOLDER/$(oc get secret es-admin -n $NAMESPACE -ojsonpath='{.data.password}' | base64 -d)/")
-echo "$bootstrap_response" | tr '\n' ' ' | sed "s/, *\"errors\" : \[ \"SSL_HANDSHAKE_EXCEPTION\" \] *} *\]/$credentials/g" > /tmp/eem-new-cluster.json
+# get the password for the Kafka cluster
+ES_PASSWORD=$(oc get secret kafka-demo-apps -n $NAMESPACE -ojsonpath='{.data.password}' | base64 -d)
+
+# substitute details into the template
+cat eem-01-cluster.json | \
+    sed "s|ES_NAMESPACE|$NAMESPACE|" | \
+    sed "s|ES_CERTIFICATE|$ES_CERTIFICATE|" | \
+    sed "s|ES_PASSWORD|$ES_PASSWORD|" > \
+    /tmp/eem-new-cluster.json
 
 # submit the cluster definition
-curl -X POST -s -k $(cookieCurlParams ${CURRENT_USER}) \
-    -H "$(csrfHeader ${CURRENT_USER})" \
-    -H "Content-Type: application/json" \
-    --data @/tmp/eem-new-cluster.json \
-    $EEM_API/api/eem/clusters
+echo "> (2/3) Submitting Event Streams cluster information"
+(
+    set +e
+
+    RESPONSE=$(curl -X POST -s -k \
+                --dump-header /tmp/eem-api-header \
+                -H 'Accept: application/json' \
+                -H 'Content-Type: application/json' \
+                -H "Authorization: Bearer $ACCESS_TOKEN" \
+                --data @/tmp/eem-new-cluster.json \
+                --output /dev/null \
+                --write-out '%{response_code}' \
+                $EEM_API/eem/clusters)
+
+    if [ $? != 0 ]
+    then
+        echo " ----------------------------------------------------------------------- "
+        echo "ERROR: Failed to connect to Event Endpoint Management"
+        exit 1
+
+    elif [ "$RESPONSE" == "409" ]
+    then
+        echo " Ignoring (cluster already exists)"
+
+    elif [[ "$RESPONSE" != \2* ]]
+    then
+        echo " ----------------------------------------------------------------------- "
+        echo "ERROR: Failed to submit Event Streams cluster information"
+        echo ""
+        cat /tmp/eem-api-header
+        exit $RESPONSE
+    fi
+)
 
 # cleanup
 rm /tmp/eem-new-cluster.json
-
 
 # --------------------------------------
 # TOPICS
@@ -189,51 +110,37 @@ rm /tmp/eem-new-cluster.json
 #  add the topics to EEM
 # --------------------------------------
 
-echo "> Submitting Kafka topics"
-
-curl -X POST -s -k $(cookieCurlParams ${CURRENT_USER}) \
-    -H "$(csrfHeader ${CURRENT_USER})" \
-    -H "Content-Type: application/json" \
-    --data @eem-02-topics.json \
-    $EEM_API/api/eem/catalogues/entries
-
-
-# --------------------------------------
-# DOCUMENT
-#
-#  document & publish the topics in EEM
-# --------------------------------------
-
-echo "> Documenting Kafka topics"
-
-curl -k -s $(cookieCurlParams ${CURRENT_USER}) \
-    $EEM_API/api/eem/topics > /tmp/eem-topics.json
+echo "> (3/3) Submitting Kafka topics"
 
 topics=("CANCELLATIONS" "CUSTOMERS.NEW" "DOOR.BADGEIN" "ORDERS.NEW" "SENSOR.READINGS" "STOCK.MOVEMENT")
 for topic in "${topics[@]}"
 do
+    RESPONSE=$(curl -X POST -s -k \
+                    --dump-header /tmp/eem-api-header \
+                    -H 'Accept: application/json' \
+                    -H 'Content-Type: application/json' \
+                    -H "Authorization: Bearer $ACCESS_TOKEN" \
+                    --data @eem-03-$topic.json \
+                    --output /dev/null \
+                    --write-out '%{response_code}' \
+                    $EEM_API/eem/catalogs/default/entries)
+    if [ "$RESPONSE" == "409" ]
+    then
+        echo " Ignoring (topic $topic already exists)"
 
-    topicid=$(grep -B 1 "\"alias\" : \"$topic\"" /tmp/eem-topics.json | grep '"id"' | cut -d '"' -f 4)
-    cat eem-03-$topic.json | sed "s/PLACEHOLDERID/$topicid/g" > /tmp/eem-topic.json
-
-    curl -X POST -s -k $(cookieCurlParams ${CURRENT_USER}) \
-        -H "$(csrfHeader ${CURRENT_USER})" \
-        -H "Content-Type: application/json" \
-        --data @/tmp/eem-topic.json \
-        $EEM_API/api/eem/catalogues/entries/$topicid
-    cat eem-04-gateway.json | sed "s/PLACEHOLDERID/$topicid/g" > /tmp/eem-gateway.json
-    curl -X POST -s -k $(cookieCurlParams ${CURRENT_USER}) \
-        -H "$(csrfHeader ${CURRENT_USER})" \
-        -H "Content-Type: application/json" \
-        --data @/tmp/eem-gateway.json \
-        $EEM_API/api/eem/publish
-
+    elif [[ "$RESPONSE" != \2* ]]
+    then
+        echo " ----------------------------------------------------------------------- "
+        echo "ERROR: Failed to submit topic $topic"
+        echo ""
+        cat /tmp/eem-api-header
+        exit $RESPONSE
+    fi
 done
 
-# cleanup
-rm /tmp/eem-topic.json
-rm /tmp/eem-gateway.json
 
+# cleanup
+rm /tmp/eem-api-header
 
 # --------------------------------------
 
